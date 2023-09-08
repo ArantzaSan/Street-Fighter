dd1a2ea16bd70e9e97859ebf81b668acda202b8b
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2023-09-08 02:05:02 +0000

    Correccion

--- include/Mascota.hpp
+++ include/Mascota.hpp
@@ -8,20 +8,20 @@ private:
 
 public:
     Mascota(std::string nombre) {
-        this ->hambre = 0;
-        this ->nombre = nombre;
+        this->hambre = 0;
+        this->nombre = nombre;
     }
     ~Mascota() {}
-    void jugar(){
-        this ->hambre += s;
+    void Jugar(){
+        this->hambre += 5;
     }
-    void comer(int comida){
-        this ->hambre -=comida
+    void Comer(int comida){
+        this->hambre -=comida;
     }
     std::string DecirNombre(){
-        return this ->nombre;
+        return this->nombre;
     }
     int DecirHambre(){
-        return this ->hambre;
+        return this->hambre;
     }
-}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@ int main( int argc, char const *argv[])
 
     Mascota m1("Chakalito");
     m1.Jugar();
-    m1,Jugar();
+    m1.Jugar();
+    m1.Jugar();
     m1.Jugar();
     
     std::cout<<m1.DecirNombre() << " tiene "
-    << m1.DecirHambre() << " de hambre." <<
+    << m1.DecirHambre() << " de hambre.";
 
     m1.Comer(5);
 
