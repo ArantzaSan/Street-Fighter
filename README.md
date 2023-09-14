251cd754e38da252f45b8b27222fb51c4d17889b
ArantzaSan <143145265+ArantzaSan@users.noreply.github.com>
2023-09-14 00:47:41 +0000

    Street Fighter1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Street-Fighter
+
+## Jugadores
+- Chun-Li
+- Ryu
+
+![](images/Street_Fighter_Logo.webp)
+
+![](images/street-fighter-1984165.webp)
+
+## Descripcion
+
+El mejor juego de peleas.
+
+# Diagrama 
+![](/out/docs/diagrama/diagrama.png)
--- include/Jugador.hpp
+++ include/Jugador.hpp
@@ -1,8 +1,52 @@
+#include <iostream>
+
+
 class Jugador
 {
 private:
-    /* data */
+int poder;
+std::string nombre;
+
+
 public:
-    Jugador(/* args */) {}
-    ~Jugador() {}
+Jugador(std::string nombre) {
+this->poder = 0;
+this->nombre = nombre;
+}
+~Jugador() {}
+void Golpear(){
+this->poder += 5;
+}
+void Daño(int daño){
+this->poder -=daño;
+}
+std::string DecirNombre(){
+return this->nombre;
+}
+int DecirPoder(){
+return this->poder;
+}
 };
+
+
+
+
+int main( int argc, char const *argv[])
+{
+std::cout<<"Street Fighter"<<std::endl;
+
+
+Jugador m1("Ryu");
+m1.Golpear();
+m1.Golpear();
+m1.Golpear();
+m1.Golpear();
+std::cout<<m1.DecirNombre() << " tiene "
+<< m1.DecirPoder() << " de poder.";
+
+
+m1.Daño(5);
+
+
+return 0;
+}
