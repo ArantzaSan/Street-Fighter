620080065ee77aa65652dab62abdc377cae09249
AldoOsuna <a22310221@live.ceti.mx>
2023-12-05 20:37:57 -0600

    Actualización

--- src/03_1_Ryu.cpp
+++ src/03_1_Ryu.cpp
@@ -2,11 +2,11 @@
 #include <SFML/Audio.hpp>
 
 
-class Personaje
+class Ryu
 {
 public:
 
-    Personaje(sf::Vector2f position, sf::Color color)
+    Ryu(sf::Vector2f position, sf::Color color)
     {
         shape.setSize(sf::Vector2f(50, 50));
         shape.setPosition(position); // Posición inicial cuadro
@@ -57,6 +57,61 @@ private:
     int frameHeight = 32;
 };
 
+class Ken
+{
+public:
+
+    Ken(sf::Vector2f position, sf::Color color)
+    {
+        shape.setSize(sf::Vector2f(50, 50));
+        shape.setPosition(position); // Posición inicial cuadro
+        shape.setFillColor(color);
+
+        // Cargar la imagen desde un archivo
+        
+        if (!texture.loadFromFile("./assets/images/ken2.0.png"))
+        {
+        
+        }
+        
+        this->sprite = sf::Sprite(texture);
+        this->sprite.setPosition(position); // Posición inicial sprite
+    }
+
+    void move(float offsetX, float offsetY)
+    {
+        sprite.move(offsetX, offsetY);
+        shape.move(offsetX, offsetY);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(this->shape);
+        window.draw(this->sprite);
+    }
+
+    void update(){
+        // Actualizar el frame de la animación
+        if (clock.getElapsedTime().asSeconds() >= frameTime)
+        {
+            currentFrame = (currentFrame + 1) % numFrames;
+            sprite.setTextureRect(sf::IntRect((currentFrame * 70) + 1208, 680, 53, 100));
+            clock.restart();
+        }
+    }
+
+private:
+    sf::RectangleShape shape;
+    sf::Sprite sprite;
+    sf::Texture texture;
+    sf::Clock clock;
+    float frameTime = 0.1f; // Tiempo entre cada frame en segundos
+    int currentFrame = 0;
+    int numFrames = 11; // Número total de frames en la animación
+    int frameWidth = 32;
+    int frameHeight = 32;
+};
+
 double velocidad = 0.1;
 
 int main()
@@ -73,7 +128,8 @@ int main()
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Street Fighter");
 
-    Personaje pika(sf::Vector2f(400, 300), sf::Color::Red);
+    Ryu pika(sf::Vector2f(200, 300), sf::Color::Red);
+    Ken pikachu(sf::Vector2f(600, 300), sf::Color::Red);
 
     while (window.isOpen())
     {
@@ -103,14 +159,35 @@ int main()
             pika.move(0, velocidad);
         }
 
+
+         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            pikachu.move(velocidad * -1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            pikachu.move(velocidad, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            pikachu.move(0, velocidad * -1);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            pikachu.move(0, velocidad);
+        }
+
+
         
 
 
         // Actualizar animacion pikachu
         pika.update();
+        pikachu.update();
 
         window.clear();
         pika.draw(window);
+        pikachu.draw(window);
         window.display();
 
         if (music.getStatus() != sf::Music::Playing)
